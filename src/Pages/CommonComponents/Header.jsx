208464a5d2b8b196919340b37d186842711e78e1
Muhammad Waqar <mohammad.waqar@xloopdigital.com>
2023-05-28 19:36:32 +0500

    header and footer done

--- src/Pages/CommonComponents/Header.jsx
+++ src/Pages/CommonComponents/Header.jsx
@@ -1,38 +1,41 @@
-import React, { useEffect, useState } from 'react'
 import logo from '../../assets/logo.png'
 import styled from 'styled-components'
 import ButtonStyle from './ButtonComponent'
 import { Link } from 'react-router-dom'
-import {IoMenuOutline} from 'react-icons/all'
 
-const Header = ({className}) => {
-    
+const Header = ({ className }) => {
+
     return (
         <header className={className}>
             <div className='margin__auto'>
-            <div className='logo'>
-                <img alt='logo' src={logo} />
-            </div>
+                <div className='logo'>
+                    <img alt='logo' src={logo} />
+                </div>
 
-            <div className="tabs">
-                <div className='tabs__item'>
-                    <Link >Pricing</Link>
-                </div>
-                <div className='tabs__item'>
-                    <Link >Work</Link>
-                </div>
-                <div className='tabs__item'>
-                    <Link >About</Link>
-                </div>
-                <div className='tabs__item__menu'>
-                    <div className='menu' >
-                        <IoMenuOutline />
+                <div className="tabs">
+                    <div className='tabs__item'>
+                        <Link >Pricing</Link>
+                    </div>
+                    <div className='tabs__item'>
+                        <Link >Work</Link>
+                    </div>
+                    <div className='tabs__item'>
+                        <Link >About</Link>
+                    </div>
+                    <div className='tabs__item__menu'>
+                        {/* menu icon */}
+                        <button className='menu'>
+                            <span className='line'></span>
+                            <span className='line'></span>
+                            <span className='line'></span>
+                        </button>
+                    </div>
+                    <div>
+                        <div className='schedule-btn'>
+                            <ButtonStyle text='Schedule a Call' variant='fill' color='yellow' ></ButtonStyle>
+                        </div>
                     </div>
                 </div>
-                <div className='schedule-btn'>
-                    <ButtonStyle text='Schedule a Call' variant='fill' color='yellow' ></ButtonStyle>
-                </div>
-            </div>
             </div>
         </header>
     )
@@ -46,42 +49,62 @@ export const HeaderStyle = styled(Header)((props) => ({
     '& .margin__auto': {
         display: 'flex',
         justifyContent: 'space-between',
-        // alignItems: 'center',
-        // height: '78px',
+        alignItems: 'center',
         maxWidth: '1140px',
         margin: 'auto',
-        paddingTop: '25px',
-        '& .logo': {
-            padding: '10px'
-        },
+        padding: '25px',
         '& .tabs': {
             display: 'flex',
-            alignItems: 'baseline',
-            // alignItems: 'center',
-            padding: '10px',
+            alignItems: 'center',
             gap: '2rem',
-            '& .tabs__item a':{
+            '& .tabs__item a': {
                 textDecoration: 'none',
                 color: props.theme.tabsColor,
                 fontWeight: '500',
             },
-            '& .tabs__item a:hover':{
-                opacity:0.8,
+            '& .tabs__item a:hover': {
+                opacity: 0.8,
             },
             '& .tabs__item__menu': {
                 display: 'none',
-                fontSize: '30px'
             }
         }
     },
-    '@media (max-width: 1030px)': {
+    '@media screen and (max-width: 767px) ': {
+        '& .tabs__item, .schedule-btn': {
+            display: 'none',
+        },
+        '& .margin__auto .tabs':{
+            gap:0,
+        }
+    },
+    '@media screen and (min-width: 768px) and (max-width: 1023px)': {
+        
+    },
+    '@media (max-width: 1023px) ': {
         '& .tabs__item': {
             display: 'none',
         },
         '& .margin__auto .tabs .tabs__item__menu': {
             display: 'block',
+        },
+        '& .tabs__item__menu .menu': {
+            width: '45px',
+            borderRadius: '3px',
+            backgroundColor: 'transparent',
+            border: '1px solid rgba(0,0,0,0.2)',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '4px',
+            padding: '8px',
+        },
+        '& .tabs__item__menu .menu .line': {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            height: '1px',
+            width: '100%',
+            display: 'block',
         }
-    }
+    },
 
 
 }))
